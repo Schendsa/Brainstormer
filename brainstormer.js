6a82350ff78fcd6bef58f6040736638a2d71a25f
Schendsa <sam.van.schendel@hotmail.nl>
2016-01-20 09:42:12 +0100

    Removed unnecessary functions

--- brainstormer.js
+++ brainstormer.js
@@ -28,9 +28,6 @@ if (Meteor.isClient) {
     },
     hideCompleted: function () {
       return Session.get("hideCompleted");
-    },
-    incompleteCount: function () {
-      return Tasks.find({checked: {$ne: true}}).count();
     }
   });
 
